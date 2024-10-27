89f9c85d7e778a5806fdf46e3e8a242f2d927a58
AaronTeah <69444412+AaronTeah@users.noreply.github.com>
2024-10-27 11:31:52 +0800

    Update EC1.py

--- EC1.py
+++ EC1.py
@@ -12,7 +12,7 @@ POP_SIZE = 500
 
 #MUT_RATE: Rate at which our string will be changed.
 #MUT_RATE = 0.2
-MUT_RATE = st.text_input("Enter your mutation rate", "0.2")
+MUT_RATE = st.number_input("Enter your mutation rate", "0.2")
 
 #TARGET: Our goal.
 #TARGET = 'aaron teah'
